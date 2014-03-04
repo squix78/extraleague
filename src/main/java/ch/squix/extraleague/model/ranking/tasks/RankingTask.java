85b33c31ac661d8c96f5c36b0be7fe5c6defc717
Daniel Eichhorn <daniel.eichhorn@netcetera.com>
2014-03-04 14:30:56 +0100

    Removed wrong copyright headers

--- src/main/java/ch/squix/extraleague/model/ranking/tasks/RankingTask.java
+++ src/main/java/ch/squix/extraleague/model/ranking/tasks/RankingTask.java
@@ -1,12 +1,3 @@
-/*
- * Copyright (C) 2014 by Netcetera AG.
- * All rights reserved.
- *
- * The copyright to the computer program(s) herein is the property of Netcetera AG, Switzerland.
- * The program(s) may be used and/or copied only with the written permission of Netcetera AG or
- * in accordance with the terms and conditions stipulated in the agreement/contract under which 
- * the program(s) have been supplied.
- */
 package ch.squix.extraleague.model.ranking.tasks;
 
 import java.util.Map;

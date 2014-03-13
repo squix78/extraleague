93e773d127826080f0b7134d4e31342de3e7f056
Daniel Eichhorn <dani.eichhorn@squix.ch>
2014-03-13 13:12:37 +0100

    Added simple circular progress bar, created with D3!

--- src/main/webapp/js/charts.js
+++ src/main/webapp/js/charts.js
@@ -9,68 +9,47 @@ angular.module('Charts', [])
       scope: {
         'ngModel': '='
       },
-      restrict: 'EA',
       link: function (scope, element, attrs) {
-        var el = D3.select('.circularProgress'),
-            w = attrs.width || el.node().clientWidth,
-            h = attrs.height || el.node().clientHeight,
-            r = Math.min(w, h) / 2,
-            pi = Math.PI;
 
-        var svg = el.select('svg')
-              .attr('width', w)
-              .attr('height', h)
-              .append('g')
-              .attr('transform', 'translate(' + w/2 + ',' + h/2 + ')');
+        
 
-        var pie = D3.layout.pie()
-          .startAngle(-pi/1.2)
-          .endAngle(pi/1.2);
+        var el = D3.select('.circularProgress');
+        var w = attrs.width || el.node().clientWidth;
+        var h = attrs.height || el.node().clientHeight;
+        var r = Math.min(w, h) / 2;
+        var pi = Math.PI;
 
+    var svgContainer = el.select('svg')
+          .attr('width', w)
+          .attr('height', h)
+          .append('g')
+          .attr('transform', 'translate(' + w/2 + ',' + h/2 + ')');
+
+        var display = svgContainer.append('text')
+                .style("text-anchor", "middle")
+                .attr('y', 5)
+		        .text((Math.round(scope.ngModel * 100)) + "%");
+        
+        var arcBackground = D3.svg.arc()
+        .innerRadius(r * 0.7)
+        .outerRadius(r * 0.9)
+        .startAngle(2 * Math.PI) //converting from degs to radians
+        .endAngle(0); //just radians
+        
+        svgContainer.append("path")
+        .style("fill", "ffffff")
+        .attr("d", arcBackground);
+        
         var arc = D3.svg.arc()
           .innerRadius(r * 0.7)
-          .outerRadius(r * 0.9);
+          .outerRadius(r * 0.9)
+          .startAngle(2 * Math.PI * scope.ngModel) //converting from degs to radians
+          .endAngle(0); //just radians
+          
+        svgContainer.append("path")
+        	.style("fill", "93cfeb")
+		    .attr("d", arc);
 
-        svg.selectAll('path.degree')
-          .data(pie(D3.range(90)
-          .map(function() { return 1; })))
-          .enter()
-            .append('path')
-            .attr('class', 'degree')
-            .attr('d', arc);
-
-        var handle = svg.append('g');
-        handle.append('rect')
-          .attr('class', 'handle')
-          .attr('width', 50)
-          .attr('height', 5)
-          .attr('y', -2.5)
-          .attr('x', r - 65);
-
-        var scale = D3.scale.linear()
-          .domain([65,85])
-          .range([-180, 0]);
-
-        var display = svg.append('text')
-            .text('72')
-            .attr('y', 25);
-
-        scope.$watch('ngModel', function(n) {
-          if (!n) { return; }
-
-          handle.transition().duration(1000)
-            .attr('transform', 'rotate('+scale(n)+')');
-
-          display.datum(n)
-            .transition()
-            .duration(1000)
-            .tween('text', function(d) {
-              var i = D3.interpolate(this.textContent, d);
-              return function(t) {
-                this.textContent = Math.round(i(t));
-              };
-            });
-        });
       }
-    };
+    };	  
   });

5990fe5b369e84afeeac9ed6be55f95c9c7a3ee6
Daniel Eichhorn <dani.eichhorn@squix.ch>
2014-03-21 14:02:15 +0100

    Added new PlayerComboDto

---
+++ src/main/java/ch/squix/extraleague/rest/ranking/PlayerComboDto.java
@@ -0,0 +1,43 @@
+package ch.squix.extraleague.rest.ranking;
+
+public class PlayerComboDto {
+	
+	private String player;
+	private String combo;
+	private Integer gamesWon = 0;
+	private Integer gamesLost = 0;
+	
+	public String getPlayer() {
+		return player;
+	}
+	public String getCombo() {
+		return combo;
+	}
+	public Integer getGamesWon() {
+		return gamesWon;
+	}
+	public Integer getGamesLost() {
+		return gamesLost;
+	}
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+	public void setCombo(String combo) {
+		this.combo = combo;
+	}
+	public void setGamesWon(Integer gamesWon) {
+		this.gamesWon = gamesWon;
+	}
+	public void setGamesLost(Integer gamesLost) {
+		this.gamesLost = gamesLost;
+	}
+	
+	public Double getSuccessRate() {
+		return 1.0 * gamesWon / (gamesWon + gamesLost);
+	}
+	
+	public Integer getTotalGames() {
+		return gamesWon + gamesLost;
+	}
+
+}

84f9fb45dd32e2ab3b0dd423c55c5bcb1376569a
Daniel Eichhorn <dani.eichhorn@squix.ch>
2014-04-01 21:04:41 +0200

    Added elo points to summary view

--- src/main/java/ch/squix/extraleague/rest/result/PlayerScoreDto.java
+++ src/main/java/ch/squix/extraleague/rest/result/PlayerScoreDto.java
@@ -5,15 +5,17 @@ public class PlayerScoreDto {
 	private String player;
 	private Integer score = 0;
 	private Integer goals = 0;
+	private Integer earnedEloPoints = 0;
 	
 	public PlayerScoreDto() {
 		
 	}
 	
-	public PlayerScoreDto(String player, Integer score, Integer goals) {
+	public PlayerScoreDto(String player, Integer score, Integer goals, Integer earnedEloPoints) {
 		this.player = player;
 		this.score = score;
 		this.goals = goals;
+		this.earnedEloPoints = earnedEloPoints;
 	}
 	
 	public String getPlayer() {
@@ -35,6 +37,14 @@ public class PlayerScoreDto {
 
 	public void setGoals(Integer goals) {
 		this.goals = goals;
+	}
+
+	public Integer getEarnedEloPoints() {
+		return earnedEloPoints;
+	}
+
+	public void setEarnedEloPoints(Integer earnedEloPoints) {
+		this.earnedEloPoints = earnedEloPoints;
 	}
 
 }
--- src/main/java/ch/squix/extraleague/rest/result/SummaryResource.java
+++ src/main/java/ch/squix/extraleague/rest/result/SummaryResource.java
@@ -25,24 +25,38 @@ public class SummaryResource extends ServerResource {
 		SummaryDto dto = new SummaryDto();
 		Map<String, Integer> playerScores = new HashMap<>();
 		Map<String, Integer> playerGoals = new HashMap<>();
+		Map<String, Integer> playerEloPoints = new HashMap<>();
 		
 		for (String player : game.getPlayers()) {
 			playerScores.put(player, 0);
 			playerGoals.put(player, 0);
+			playerEloPoints.put(player, 0);
 		}
 		for (Match match : matches) {
 			Integer teamAScore = match.getTeamAScore();
 			Integer teamBScore = match.getTeamBScore();
 			String [] winners = null;
+			String [] losers = null;
+			Integer winPoints = 0;
 			if (teamAScore > teamBScore) {
 				winners = match.getTeamA();
+				losers = match.getTeamB();
+				winPoints = match.getWinPointsTeamA();
 			} else {
 				winners = match.getTeamB();
+				losers = match.getTeamA();
+				winPoints = match.getWinPointsTeamB();
 			}
 			for (String winner : winners) {
 				Integer score = playerScores.get(winner);
 				score++;
 				playerScores.put(winner, score);
+				Integer eloPoints = playerEloPoints.get(winner);
+				playerEloPoints.put(winner, eloPoints + winPoints);
+			}
+			for (String loser : losers) {
+				Integer eloPoints = playerEloPoints.get(loser);
+				playerEloPoints.put(loser, eloPoints - winPoints);
 			}
 			for (String player : playerScores.keySet()) {
 				Integer goals = playerGoals.get(player);
@@ -51,7 +65,10 @@ public class SummaryResource extends ServerResource {
 			}
 		}
 		for (String player : game.getPlayers()) {
-			dto.getPlayerScores().add(new PlayerScoreDto(player, playerScores.get(player), playerGoals.get(player)));
+			dto.getPlayerScores().add(new PlayerScoreDto(
+					player, playerScores.get(player), 
+					playerGoals.get(player), 
+					playerEloPoints.get(player)));
 		}
 		return dto;
 	}

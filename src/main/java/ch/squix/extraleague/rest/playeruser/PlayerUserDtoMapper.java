2539f689d267554b7200ebbb4e9f0bf2c06bea7b
Daniel Eichhorn <dani.eichhorn@squix.ch>
2014-04-15 22:19:52 +0200

    Added servlet to cache and resize the player pictures

--- src/main/java/ch/squix/extraleague/rest/playeruser/PlayerUserDtoMapper.java
+++ src/main/java/ch/squix/extraleague/rest/playeruser/PlayerUserDtoMapper.java
@@ -1,5 +1,7 @@
 package ch.squix.extraleague.rest.playeruser;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,18 +9,28 @@ import ch.squix.extraleague.model.match.player.PlayerUser;
 
 public class PlayerUserDtoMapper {
 	
+	private final static String MAPPING_SERVICE_URL = "/playerImage?url=";
+	
 	public static PlayerUserDto mapToDto(PlayerUser playerUser) {
 		PlayerUserDto dto = new PlayerUserDto();
 		dto.setPlayer(playerUser.getPlayer());
 		dto.setEmail(playerUser.getEmail());
-		dto.setImageUrl(playerUser.getImageUrl());
+		String imageUrl = playerUser.getImageUrl();
+		if (imageUrl != null) {
+			try {
+				String imageUrlEncoded = URLEncoder.encode(imageUrl, "utf-8");
+				dto.setImageUrl(MAPPING_SERVICE_URL + imageUrlEncoded);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		}
 		return dto;
 	}
 	
 	public static PlayerUserDto mapToAdminDto(PlayerUser playerUser) {
 		PlayerUserDto dto = mapToDto(playerUser);
 		dto.setEmail(playerUser.getEmail());
-		dto.setImageUrl(playerUser.getImageUrl());
+
 		dto.setEmailNotification(playerUser.getEmailNotification());
 		return dto;
 	}
---
+++ src/main/java/ch/squix/extraleague/servlets/PlayerImageServlet.java
@@ -0,0 +1,72 @@
+package ch.squix.extraleague.servlets;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.images.Image;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesService.OutputEncoding;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.Transform;
+
+/**
+ * Creates thumbnails by calling another service
+ * 
+ * @author deichhor
+ * 
+ */
+public class PlayerImageServlet extends HttpServlet {
+
+	public static final String THUMBNAIL_SERVICE_URL = "http://worker.feederator.org:8080/FeedShots/shoot?url=";
+	private static final Logger log = Logger.getLogger(PlayerImageServlet.class.getName());
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String urlString = req.getParameter("url");
+		
+		resp.setContentType("image/png");
+		BufferedInputStream bis = null;
+		ByteArrayOutputStream bos = null;
+		try {
+			URL url = new URL(urlString);
+			URLConnection connection = url.openConnection();
+			connection.setConnectTimeout(20000);
+			connection.setReadTimeout(20000);
+
+			bis = new BufferedInputStream(connection.getInputStream());
+			bos = new ByteArrayOutputStream();
+			byte[] buffer = new byte[0xFFFF];
+			for (int len; (len = bis.read(buffer)) != -1;) {
+				bos.write(buffer, 0, len);
+			}
+			byte[] picture = bos.toByteArray();
+			// only set the cache header after successfull header reading
+			resp.setHeader("Cache-Control", "public, max-age=86400");
+
+			ImagesService imagesService = ImagesServiceFactory.getImagesService();
+
+			Image oldImage = ImagesServiceFactory.makeImage(picture);
+			Transform resize = ImagesServiceFactory.makeResize(48, 58, 0.5, 0);
+			Image newImage = imagesService.applyTransform(resize, oldImage, OutputEncoding.PNG);
+			picture = newImage.getImageData();
+
+			resp.getOutputStream().write(picture);
+		} catch (Exception e) {
+			System.err.println(e);
+			resp.setHeader("Cache-Control", "no-cache,must-revalidate");
+			resp.sendRedirect(resp.encodeRedirectURL("/images/person2.png"));
+		}
+
+	}
+
+}

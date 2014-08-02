abd55c137e75b495f6ad74a6ebe412ab1dc4c283
Daniel Eichhorn <dani.eichhorn@squix.ch>
2014-08-02 15:31:07 +0200

    Fixed NPE for old games without maxGoals or maxMatches

--- src/main/java/ch/squix/extraleague/rest/games/GameDtoMapper.java
+++ src/main/java/ch/squix/extraleague/rest/games/GameDtoMapper.java
@@ -19,8 +19,16 @@ public class GameDtoMapper {
 		dto.setGameProgress(game.getGameProgress());
 		dto.setFirstGoalDate(game.getFirstGoalDate());
 		dto.setIsGameFinished(game.getIsGameFinished());
-		dto.setMaxMatches(game.getMaxMatches());
-		dto.setMaxGoals(game.getMaxGoals());
+		Integer maxMatches = game.getMaxMatches();
+		if (maxMatches == null) {
+			maxMatches = 4;
+		}
+		Integer maxGoals = game.getMaxGoals();
+		if (maxGoals == null) {
+			maxGoals = 5;
+		}
+		dto.setMaxMatches(maxMatches);
+		dto.setMaxGoals(maxGoals);
 		dto.setGameMode(game.getGameMode());
 		if (game.getFirstGoalDate() != null && game.getEndDate() == null) {
 			Double progress = game.getGameProgress();
@@ -31,15 +39,15 @@ public class GameDtoMapper {
 			Long durationSinceFirstGoal =  now - game.getFirstGoalDate().getTime();
 			// only calculate
 			if (durationSinceFirstGoal > 10000 && progress >= 0.1 ) {
-				Double goalTimeShare = 1.0d / (game.getMaxMatches() * game.getMaxGoals());
+				Double goalTimeShare = 1.0d / (maxMatches * maxGoals);
 				Long estimatedRemainingMillis = Math.round((1 - progress) * durationSinceFirstGoal / (progress - goalTimeShare));
 				dto.setEstimatedRemainingMillis(estimatedRemainingMillis);
 			} else {
 				// heuristic;-) A match usually takes between 350s
-				dto.setEstimatedRemainingMillis(AVERAGE_MILLIS_BETWEEN_GOALS * game.getMaxMatches() * game.getMaxGoals());
+				dto.setEstimatedRemainingMillis(AVERAGE_MILLIS_BETWEEN_GOALS * maxMatches * maxGoals);
 			}
 		} else {
-			dto.setEstimatedRemainingMillis(AVERAGE_MILLIS_BETWEEN_GOALS * game.getMaxMatches() * game.getMaxGoals());
+			dto.setEstimatedRemainingMillis(AVERAGE_MILLIS_BETWEEN_GOALS * maxMatches * maxGoals);
 		}
 		return dto;
 	}

6b7425921e7f24fcb4f263aa986a48fd1b6f95e1
Daniel Eichhorn <dani.eichhorn@squix.ch>
2014-08-02 14:15:30 +0200

    Redisign of new game view

--- src/main/webapp/js/extraleague.js
+++ src/main/webapp/js/extraleague.js
@@ -1,4 +1,4 @@
-angular.module('Extraleague', ['ngResource', 'ngRoute', 'ngTouch', 'PlayerMappings', 'Charts', 'Games', 'ui.bootstrap', 'nvd3ChartDirectives', 'gaeChannelService'])
+angular.module('Extraleague', ['ngResource', 'ngRoute', 'ngTouch', 'PlayerMappings', 'Charts', 'Games', 'ui.bootstrap', 'ui.bootstrap.buttons', 'nvd3ChartDirectives', 'gaeChannelService'])
     .config(function($routeProvider) {
         $routeProvider
         .when('/newGame', {

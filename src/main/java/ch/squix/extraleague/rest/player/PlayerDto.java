bfc9fda3f218117b7483f654d35a8fa7bcf2a51c
Daniel Eichhorn <dani.eichhorn@squix.ch>
2014-09-04 21:13:29 +0200

    added eternal rankings

--- src/main/java/ch/squix/extraleague/rest/player/PlayerDto.java
+++ src/main/java/ch/squix/extraleague/rest/player/PlayerDto.java
@@ -13,7 +13,9 @@ public class PlayerDto {
 	private String player;
 	private RankingDto statistics;
 	private RankingDto dayEndStatistics;
+	private RankingDto eternalStatistics;
 	private List<MatchDto> lastMatches = new ArrayList<>();
+	
 	
 	public String getPlayer() {
 		return player;
@@ -41,7 +43,13 @@ public class PlayerDto {
 	public void setDayEndStatistics(RankingDto dayEndStatistics) {
 		this.dayEndStatistics = dayEndStatistics;
 	}
+	public RankingDto getEternalStatistics() {
+		return eternalStatistics;
+	}
 
+	public void setEternalStatistics(RankingDto eternalStatistics) {
+		this.eternalStatistics = eternalStatistics;
+	}
 
 
 }

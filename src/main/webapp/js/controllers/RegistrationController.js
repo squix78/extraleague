11c2fdeb7b829f6cef6a81d8d7f72e61acbd10ca
Daniel Eichhorn <dani.eichhorn@squix.ch>
2014-10-13 06:09:07 +0200

    Continued work on registration

--- src/main/webapp/js/controllers/RegistrationController.js
+++ src/main/webapp/js/controllers/RegistrationController.js
@@ -2,18 +2,24 @@ angular.module('Extraleague').controller('RegistrationController',
 function($scope, $http, Blobs, PlayerService, PlayerUsers) {
 	$scope.blobUrl = Blobs.get({});
 	$scope.player = new PlayerUsers();
+	$scope.isRegistrationOK = false;
+	$scope.isRegistrationFailed = false;
 	$scope.enableCam = function() {
 		$scope.showCam = true;
 	};
 	
 	$scope.register = function() {
+		$scope.isRegistrationFailed = false;
+		$scope.isRegistrationOK = false;
 		$scope.isPlayerRegistering = true;
 		$scope.tmpPlayer = PlayerService.savePlayer($scope.player);
 		$scope.tmpPlayer.then(function(result) {
 			$scope.isPlayerRegistering = false;
 			$scope.player = new PlayerUsers();
+			$scope.isRegistrationOK = true;
 		}, function(error) {
 			console.log("Error saving player: " + error)
+			$scope.isRegistrationFailed = true;
 		});
 	};
 	

146d26fd567178df4bab7903d6fdffd9b30e320e
Daniel Eichhorn <dani.eichhorn@squix.ch>
2014-10-16 13:55:47 +0200

    Tried to fix picture orientation after ios upload

--- src/main/java/ch/squix/extraleague/servlets/PlayerImageServlet.java
+++ src/main/java/ch/squix/extraleague/servlets/PlayerImageServlet.java
@@ -1,6 +1,7 @@
 package ch.squix.extraleague.servlets;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -12,10 +13,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifIFD0Directory;
 import com.google.appengine.api.images.Image;
 import com.google.appengine.api.images.ImagesService;
 import com.google.appengine.api.images.ImagesService.OutputEncoding;
 import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.OutputSettings;
 import com.google.appengine.api.images.Transform;
 
 /**
@@ -49,6 +54,7 @@ public class PlayerImageServlet extends HttpServlet {
 				bos.write(buffer, 0, len);
 			}
 			byte[] picture = bos.toByteArray();
+			picture = rotateImage(picture);
 			// only set the cache header after successfull header reading
 
 			ImagesService imagesService = ImagesServiceFactory.getImagesService();
@@ -65,6 +71,46 @@ public class PlayerImageServlet extends HttpServlet {
 			resp.sendRedirect(resp.encodeRedirectURL("/images/person2.png"));
 		}
 
+	}
+	private int getEXIFOrientation(byte[] bytes) {
+		int orientation = -1; // default = unknown
+		 ByteArrayInputStream bis = null;
+		 try {
+		 bis = new ByteArrayInputStream(bytes);
+		 Metadata metadata = ImageMetadataReader.readMetadata(new BufferedInputStream(bis), false);
+		 ExifIFD0Directory exifDir = metadata.getDirectory(ExifIFD0Directory.class);
+		 if (exifDir != null) {
+		 orientation = exifDir.getInt(274); // 274 is the EXIF orientation standard code
+		 }
+		 } catch (Exception e) {
+		 log.warning("Couldn't extract EXIF orientation from image");
+		 } finally {
+		 if (bis != null) try {
+		 bis.close();
+		 } catch (IOException e) {
+		 // nothing
+		 }
+		 }
+		 return orientation;
+	}
+	
+	private byte[] rotateImage(byte[] bytes) {
+		byte[] result = bytes;
+		int orientation = getEXIFOrientation(bytes);
+		int degrees = -1;
+		if (orientation == 3) degrees = 180;
+		else if (orientation == 6) degrees = 90;
+		else if (orientation == 8) degrees = -90;
+		if (degrees != -1) {
+		Image img = ImagesServiceFactory.makeImage(bytes);
+		Transform rotation = ImagesServiceFactory.makeRotate(degrees);
+		// GAE changes output format from JPEG to PNG while rotating the image if the expected output format is not given:
+		OutputSettings settings = new OutputSettings(ImagesService.OutputEncoding.JPEG);
+		settings.setQuality(1);
+		img = ImagesServiceFactory.getImagesService().applyTransform(rotation, img, settings);
+		result = img.getImageData();
+		}
+		return result;
 	}
 
 }
